b0ac64a14793569f0fdab97539ee16815f4a3b83
Vaibhav Singh Chandel <93109628+stonedseeker@users.noreply.github.com>
2022-05-06 18:51:56 +0530

    Update problem#07.py

--- ActivitySet#1/problem#07.py
+++ ActivitySet#1/problem#07.py
@@ -1,3 +1,20 @@
 # Strings
 
-text = "X-DSPAM-Confidence:    0.8475"
+# text = "X-DSPAM-Confidence:    0.8475"
+fname = input("Enter File Name")
+fh = open(fname)
+count = 0
+total = 0
+for line in fh:
+    if line.startswith("X-DSPAM-Confidence:"):
+        count += 1
+        x = line.find(":")
+        y = line[x+1:]
+        s = float(y)
+        total = total + s 
+        continue
+    average = total / count
+
+    print("Average spam confidence:",average)
+
+fh.close

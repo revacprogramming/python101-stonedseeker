1dfc6c80f0b733b984f56dad56804d04aa294b77
Vaibhav Singh Chandel <93109628+stonedseeker@users.noreply.github.com>
2022-05-14 08:27:55 +0530

    Update problem#11.py

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,26 @@
 # Tuples
 
-filename = "dataset/mbox-short.txt"
+#filename = "dataset/mbox-short.txt"
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+
+di = {}
+
+for line in handle:
+    if line.startswith("From "):
+        
+        line = line.split()
+        line = line[5]
+        line = line[0:2]
+        di[line]=di.get(line,0)+1
+        
+lst = list()
+for v,c in di.items():
+    lst.append((v,c))
+
+lst.sort()
+
+for v,c in lst:    
+ print(v,c)

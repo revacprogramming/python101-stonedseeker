fcb32e2572d127c58b7ec1c8b9fdd6db183f59eb
Vaibhav Singh Chandel <93109628+stonedseeker@users.noreply.github.com>
2022-05-16 21:50:16 +0530

    Update problem#14.py

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -1,41 +1,45 @@
-# Using Web Services
-# https://www.py4e.com/lessons/servces
 # # Using Web Services
 # # https://www.py4e.com/lessons/servces
 
-# import urllib.request, urllib.parse, urllib.error
-# import re
-# from bs4 import BeautifulSoup
-# #counts = dict()
-# fhand = urllib.request.urlopen('http://py4e-data.dr-chuck.net/comments_1397976.html')
+import urllib.request, urllib.parse, urllib.error
+import re
+from bs4 import BeautifulSoup
+#counts = dict()
+fhand = urllib.request.urlopen('http://py4e-data.dr-chuck.net/comments_1397976.html')
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+
+url = 'http://py4e-data.dr-chuck.net/comments_1397976.html'
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+tags = soup('span')
+
+for tag in tags:
+    numbers = re.findall('[0-9]+',str(tags))
+
+sum = 0
+for i in range(0,len(x)):
+    sum = sum + int(numbers[i])
+
+print(sum)
+
 # print(str(fhand))
 
 
-# # numbers = re.findall('[0-9]+',fhand)
-# # print(numbers)
+# numbers = re.findall('^span [0-9]+',str(fhand))
+# print(numbers)
 
-# # sum = 0
+# sum = 0
 
-# # for i in range(0,len(numbers)):
-# #     sum += int(numbers[i])
+# for i in range(0,len(numbers)):
+#     sum += int(numbers[i])
 
-# # print(sum)
+# print(sum)
 
 # # for line in fhand:
 # #     words = line.decode().strip()
 # #     for word in words:
 # #         counts[word] = counts.get(word, 0) + 1
 # # print(counts)
-
-
-import urllib.request, urllib.parse, urllib.error
-from bs4 import BeautifulSoup
-
-url = 'http://www.dr-chuck.com/page1.html'
-html = urllib.request.urlopen(url).read()
-soup = BeautifulSoup(html, 'html.parser')
-
-tags = soup('a')
-
-for tag in tags:
-    print(tag.get('href',None))

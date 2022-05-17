0d71e98ec871fd8d0d46f3c42c3bebbb509b95a7
Vaibhav Singh Chandel <93109628+stonedseeker@users.noreply.github.com>
2022-05-17 07:36:34 +0530

    Update problem#04.py

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -3,7 +3,6 @@ rate = float(input("Enter rate"))
 
 if hrs <= 40:
  pay = hrs * rate
+else: pay = (40 * rate + 1.5 * rate * (hrs - 40))
 
-pay = (40 * rate + 1.5 * rate * (hrs - 40))
-
-print("PAY= ",pay)
+print(pay)

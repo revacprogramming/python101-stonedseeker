05afdbcc553d787bfe6db917fee765c0aabb0367
Vaibhav Singh Chandel <93109628+stonedseeker@users.noreply.github.com>
2022-05-24 19:19:39 +0530

    Update problem#16.py

--- ActivitySet#1/problem#16.py
+++ ActivitySet#1/problem#16.py
@@ -1,2 +1,21 @@
 # Databases
 # https://www.py4e.com/lessons/database
+
+import urllib.request
+import xml.etree.ElementTree as ET
+
+url = "http://py4e-data.dr-chuck.net/comments_1397978.xml"
+
+data = urllib.request.urlopen(url).read().decode()
+
+tree = ET.fromstring(data)
+
+counts = tree.findall('.//count')
+
+lst = list()
+
+for i in counts:
+    num = int(i.text)
+    lst.append(num)
+
+print(f"sum = {sum(lst)}")

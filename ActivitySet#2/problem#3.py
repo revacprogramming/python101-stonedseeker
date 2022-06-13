ab9438b61506bdba3ce15d7c247441e356c4c90e
Vaibhav Singh Chandel <vaibhav24k@gmail.com>
2022-06-13 15:49:24 +0530

    done 3

--- ActivitySet#2/problem#3.py
+++ ActivitySet#2/problem#3.py
@@ -1,23 +1,17 @@
 
-
 def get_cs():
     cs = input("Enter a string\n")
     return cs
 
-list = []
-
+lst = list()
 def cs_to_lot(get_cs):
-        split = get_cs.split()
-        for i in lst:
-            lst = split[i]
-        print(lst)
+        return list(get_cs.split(" "))
 
 def main():
     cs = get_cs()
-
     lot = cs_to_lot(cs)
-    print(lot)
 
+    print(lot)
 
 if __name__ == '__main__':
     main()

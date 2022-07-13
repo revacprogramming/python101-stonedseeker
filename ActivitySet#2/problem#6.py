b812e66512095798ca7a90516f97fbd47017814b
Vaibhav Singh Chandel <vaibhav24k@gmail.com>
2022-07-13 09:35:37 +0530

    Done 6

--- ActivitySet#2/problem#6.py
+++ ActivitySet#2/problem#6.py
@@ -1,20 +1,20 @@
 class Menu:
-    def add(self, add,num):
-        self.add = add
-        self.num = num
-        
+    dir = {}
+    def add(self, name,cost):
+        self.dir[name] = cost
+    
     def show(self):
-        return self.add
+        for i in self.dir:
+            print(i,self.dir[i])
 
 
 m = Menu()  # Menu is a class
-m.add
-m.add("vada")
-
-print(m.show())
+m.add("Idly", 10)
+m.add("Vada",20)
 
 
-#adas
+m.show()
+
 ###Learinig Classes 
 
 
@@ -46,3 +46,39 @@ print(m.show())
 #     def setcolor(self, color):
 #         self7
 
+
+# class Menu:
+#     def add(self, add,num):
+#         self.add = add
+#         self.num = num
+        
+#     def show(self):
+#         return self.add
+
+
+# m = Menu()  # Menu is a class
+# m.add("idly", 10)
+# m.add("vada")
+
+# print(m.show())
+
+
+
+###Learinig Classes 
+
+
+# class dog:
+#     attr1 = "mammal"
+#     attr2 = "dog"
+
+#     def fun(self):
+#         print("I'm a", self.attr1)
+#         print("I'm a", self.attr2)
+
+# Rodger = dog()
+
+# print(Rodger.attr1)
+# Rodger.fun()
+
+# class Menu:
+#     def add("")
